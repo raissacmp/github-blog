e39973f512357130ae0e89aa352ae6bebc7052ec
raissa campos <raissa.campos26@gmail.com>
2023-04-12 14:39:08 +0100

    feat: API consumption

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,13 +1,22 @@
-import axios from "axios";
+import { api } from "../../lib/axios";
 import { useEffect, useState } from "react";
 
+interface Profile {
+  avatar_url: string;
+  name: string;
+  bio: string;
+  login: string;
+  location: string;
+  followers: string;
+}
+
 export function Profile() {
-  const [dataProfile, setDataProfile] = useState({});
+  const [dataProfile, setDataProfile] = useState<Profile>();
   console.log("🚀 ~ file: index.tsx:6 ~ Profile ~ dataProfile:", dataProfile);
 
   async function getUserData() {
-    const url = "https://api.github.com/users/raissacmp";
-    const response = await axios.get(url);
+    const url = "/users/raissacmp";
+    const response = await api.get(url);
 
     setDataProfile(response.data);
   }
@@ -15,4 +24,26 @@ export function Profile() {
   useEffect(() => {
     getUserData();
   }, []);
+
+  return (
+    <main>
+      {dataProfile && (
+        <section>
+          <div>
+            <img src={dataProfile.avatar_url} alt="Avatar Github" />
+          </div>
+          <div>
+            <h1>{dataProfile.name}</h1>
+            <a href="https://github.com/raissacmp">GITHUB</a>
+          </div>
+          <p>{dataProfile.bio}</p>
+          <div>
+            <span>{dataProfile.login}</span>
+            <span>{dataProfile.location}</span>
+            <span>{dataProfile.followers}</span>
+          </div>
+        </section>
+      )}
+    </main>
+  );
 }
